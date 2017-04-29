1931d3a91e5431a526773c4d430db247fac342d7
BuildTools <unconfigured@null.spigotmc.org>
2017-04-29 15:57:51 -0500

    Added a msgCentered()

--- src/me/messageofdeath/PaidRanks/Utils/zRequired/Commands/MessageCommand.java
+++ src/me/messageofdeath/PaidRanks/Utils/zRequired/Commands/MessageCommand.java
@@ -50,4 +50,13 @@ public abstract class MessageCommand {
 	public void noPerm(CommandSender cmd) {
 		error(cmd, LanguageSettings.Commands_NoPermission.getSetting());
 	}
+	
+	public void msgCenter(IssuedCommand cmd, String msg) {
+		String space = "                                                                              ";
+		String text1 = msg.replace("&0", "").replace("&1", "").replace("&2", "").replace("&3", "").replace("&4", "").replace("&5", "").replace("&6", "").replace("&7", "")
+				.replace("&8", "").replace("&8", "").replace("&9", "").replace("&a", "").replace("&b", "").replace("&c", "").replace("&d", "").replace("&e", "")
+				.replace("&f", "").replace("&k", "").replace("&l", "").replace("&m", "").replace("&n", "").replace("&o", "").replace("&r", "");
+		String output = space.substring((space.length() + text1.length()) / 2, space.length()) + msg;
+		msg(cmd, output);
+	}
 }
